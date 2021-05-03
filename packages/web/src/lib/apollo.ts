16918687887649fc36302fe0b70bfca7b93c7b26
Tim O'Connell <hi@timoconnell.co.uk>
2021-05-04 02:32:52 +0800

    sanity env vars for apollo client

--- packages/web/src/lib/apollo.ts
+++ packages/web/src/lib/apollo.ts
@@ -1,19 +1,22 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { SanityProjectDetails } from '../types/sanity.types';
 
-const SANITY_PROJECT_ID = 'n4kphzu5';
-const SANITY_DATASET = 'dev';
-const SANITY_TAG = 'default';
-const SANITY_USE_CDN = false;
-// eslint-disable-next-line max-len
-const SANITY_TOKEN = 'skH4iyPeo4EYaoIshd9MoHRZMpHZPU3wIb9H2bY4N3V2yihnQreIqT4huZ0x2sakfT5emgcwmlBAlgqFRVIt7YA45vuOzmfXsF6mbooIK5KwSbULLiUZbC3JYTI5KPS6XWzA6f352rud4YELUgaqfFEsfu3pux888h4zmw2SiFZ5OG8lRkJw';
+const {
+  SANITY_PROJECT_ID = 'n4kphzu5',
+  SANITY_DATASET = 'dev',
+  SANITY_TAG = 'default',
+  SANITY_USE_CDN = 'false',
+  SANITY_TOKEN,
+} = process.env;
 
 export const SANITY_PROJECT: SanityProjectDetails = {
   projectId: SANITY_PROJECT_ID,
   dataset: SANITY_DATASET,
 };
 
-const SANITY_BASE_URL = `https://${SANITY_PROJECT_ID}.${SANITY_USE_CDN ? 'apicdn' : 'api'}.sanity.io`;
+const useCdn = SANITY_USE_CDN === 'true';
+
+const SANITY_BASE_URL = `https://${SANITY_PROJECT_ID}.${useCdn ? 'apicdn' : 'api'}.sanity.io`;
 
 const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
